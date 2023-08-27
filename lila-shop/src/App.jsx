4737c9b749905534f028f1a1ab64a2a6448577ff
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2023-08-27 13:02:32 +0200

    creating the initial folders

---
+++ lila-shop/src/App.jsx
@@ -0,0 +1,15 @@
+
+import '../src/styles/App.scss'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>this is a heading</h1>
+      <button>click here</button>
+    </>
+  )
+}
+
+export default App
